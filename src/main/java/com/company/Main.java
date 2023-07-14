d5486e3e2ed9bc981fc94c06e3d5dfabd7a4bf7a
keerthana <keerthana.namulametu@careeco.net>
2023-07-14 23:40:24 +0530

    Initial commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+import com.company.clip04.SalesDataService;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        SalesDataService service = new SalesDataService();
+        List list = service.processData("C:\\Users\\keerthana_careeco\\Downloads\\java-fundamentals-exception-handling\\05-working-with-exceptions\\working-with-exceptions-exercise-files\\m4\\bad.txt");
+        System.out.println(list);
+    }
+}
